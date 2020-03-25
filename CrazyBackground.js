fc073fabcb8219641c175151a0d135ffacd933f7
Guilherme <31912784+gui25@users.noreply.github.com>
2020-03-25 17:40:53 -0300

    Add files via upload

---
+++ CrazyBackground.js
@@ -0,0 +1,13 @@
+document.addEventListener("click", function(){
+    flag = 1;
+});
+document.addEventListener("dblclick", function(){
+    flag = 0;
+});
+
+var intervalo = window.setInterval(function() {
+    if(flag == 1){
+    cor = "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
+    document.body.style.backgroundColor = cor;
+    }
+}, 500);
