93ab2c3c92c338dd2b72591655370ffa0c5c5d74
Guilherme <31912784+gui25@users.noreply.github.com>
2020-03-25 18:35:12 -0300

    Add files via upload

--- CrazyBackground.js
+++ CrazyBackground.js
@@ -9,5 +9,11 @@ var intervalo = window.setInterval(function() {
     if(flag == 1){
     cor = "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
     document.body.style.backgroundColor = cor;
+
+    cor = "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
+    document.getElementById("left").style.backgroundColor = cor;
+    
+    cor = "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
+    document.getElementById("right").style.backgroundColor = cor;
     }
 }, 500);
