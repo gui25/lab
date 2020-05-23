7106ebc184d895a6ed97f3d64b79b32a2d4fd057
Guilherme <gui25ber@gmail.com>
2020-05-22 23:36:56 -0300

    Update jquery.particleground.js

--- particle/jquery.particleground.js
+++ particle/jquery.particleground.js
@@ -379,7 +379,7 @@
     maxSpeedY: 0.7,
     directionX: 'center', // 'center', 'left' or 'right'. 'center' = dots bounce off edges
     directionY: 'center', // 'center', 'up' or 'down'. 'center' = dots bounce off edges
-    density: 5000, // How many particles will be generated: one particle every n pixels
+    density: 3000, // How many particles will be generated: one particle every n pixels
     dotColor: '#666666',
     lineColor: '#666666',
     particleRadius: 7, // Dot size
